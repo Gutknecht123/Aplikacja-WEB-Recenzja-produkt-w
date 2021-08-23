eaa1490296d2c6f3483b64320bea0efcb9dac17b
Robert Kołoszuk <76182001+Gutknecht123@users.noreply.github.com>
2021-08-23 22:57:00 +0200

    Initial commit

---
+++ node/models/test.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+
+
+
+});
+
+
+module.exports.Test = mongoose.model('Test', postSchema);
---
+++ node/server/routes/api/posts.js
@@ -0,0 +1,71 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongodb = require('mongodb');
+
+
+const router = express.Router();
+
+/*
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://mongo:5000/posts', {useNewUrlParser: true})
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'CONNECTION ERROR'));
+
+db.once('open', function(){
+    console.log('Connected');
+});
+*/
+
+//getposts
+
+router.get('/', async (req, res) => {
+
+    const posts = await dbconnect();
+    res.send(await posts.find({}).toArray());
+
+});
+
+
+//addposts
+
+router.post('/', async (req,res) => {
+
+    const posts = await dbconnect();
+    await posts.insertOne({
+        text: req.body.text,
+        category: req.body.category,
+        likes: req.body.likes,
+        //comms: 0,
+        media: req.body.media,
+        stars: req.body.stars,
+        creator: req.body.creator,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+
+});
+
+//delposts
+
+router.delete('/:id', async (req,res) =>{
+    const posts = await dbconnect();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+});
+
+
+async function dbconnect(){
+
+    const client = await mongodb.MongoClient.connect('mongodb+srv://user:12345@cluster0.rorub.mongodb.net/mongodb?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+return client.db('mongodb').collection('posts');
+
+}
+
+module.exports = router;
